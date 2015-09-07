4f83da65925b7d5a7dbca978570f06fd98121fb7
Joel Handwell <joelhandwell@gmail.com>
2015-09-07 16:52:46 -0400

    Update firewall cookbook to 2.0.1

--- spec/w_haproxy/keepalived_spec.rb
+++ spec/w_haproxy/keepalived_spec.rb
@@ -4,21 +4,20 @@ describe 'w_haproxy::keepalived' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new.converge(described_recipe)
   end
-  
+
   it 'runs recipe keepalived' do
     expect(chef_run).to include_recipe('keepalived')
-  end  
+  end
 
   it 'creates a template /etc/keepalived/keepalived.conf' do
     expect(chef_run).to create_template('/etc/keepalived/keepalived.conf')
   end
-    
+
   it 'enables firewall' do
-  	expect(chef_run).to enable_firewall('ufw')
+  	expect(chef_run).to install_firewall('default')
   end
 
 	it "runs resoruce firewall_rule to allow vrrp protocol" do
-  	expect(chef_run).to allow_firewall_rule('vrrp').with(provider: Chef::Provider::FirewallRuleIptables, protocol: 112)
+  	expect(chef_run).to create_firewall_rule('vrrp').with(provider: Chef::Provider::FirewallRuleIptables, protocol: 112)
   end
-  
 end
